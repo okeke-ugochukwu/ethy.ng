bf1331266d3d1871ba4911b1693ca1b078013c52
Ugo <success.us7@gmail.com>
2021-03-23 02:47:13 +0100

    Done nav structure, js not working well yet

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+document.querySelector(".open-btn").addEventListener("click", function() {
+    if (document.querySelector("#nav_UL").style.marginLeft = "-300px") {
+        document.querySelector("#nav_UL").style.marginLeft = "0";
+    }
+    else {
+        document.querySelector("#nav_UL").style.marginLeft = "-300px";
+
+    }
+   
+})
