2e11768fdb6d34f58aecca33c9602aaff7f67692
Ugo <success.us7@gmail.com>
2021-03-23 16:25:17 +0100

    nav now working

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-
-document.querySelector(".open-btn").addEventListener("click", function() {
-    if (document.querySelector("#nav_UL").style.marginLeft = "-300px") {
-        document.querySelector("#nav_UL").style.marginLeft = "0";
-    }
-    else {
-        document.querySelector("#nav_UL").style.marginLeft = "-300px";
-
-    }
-   
+$(".open-btn").click(function() {
+    $("#nav_UL").toggleClass("full");
 })
