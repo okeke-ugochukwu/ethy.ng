642a6aa8e81d19a99307d1a680db9c47c388fc72
Ugo <success.us7@gmail.com>
2021-03-30 10:31:27 +0100

    Slideshow now working with breadcrumbs

--- js/script.js
+++ js/script.js
@@ -1,55 +1,70 @@
-
-//show or hide menu option
+//show or hide menu option 
 $(".open-btn").click(function() {
     $("#nav_UL").toggleClass("full");
 })
 
-//change silde
-$("#sb1").click(function() {
-    $(".sb").css("background", "#ffffffb5");
-    $(this).css("background", "#412718");
-    $(".slideshow_slider").css("margin-left", "0%");
-
-})
-$("#sb2").click(function() {
-    $(".sb").css("background", "#ffffffb5");
-    $(this).css("background", "#412718");
-    $(".slideshow_slider").css("margin-left", "-100%");
-})
-$("#sb3").click(function() {
-    $(".sb").css("background", "#ffffffb5");
-    $(this).css("background", "#412718");
-    $(".slideshow_slider").css("margin-left", "-200%");
-})
-$("#sb4").click(function() {
-    $(".sb").css("background", "#ffffffb5");
-    $(this).css("background", "#412718");
-    $(".slideshow_slider").css("margin-left", "-300%");
-})
 
 //automatic sliding
+//I did not plan/reserch this properly berfore coding, code should have been alot shorter 
+function moveSlider() {
+   var sliderMargin = document.getElementById("slideshow_slider").style.marginLeft;
 
-var counter = 1
-function moveslide() {
-   var eu = document.getElementById("slideshow_slider").style.marginLeft;
-   console.log(eu);
+    switch (sliderMargin) {
+        case "-300%":
+            hightLightFirstBreadcrumb();
+            break;
 
-    if (eu != "-300%") {
-        var eu_numerial_value = (eu.slice(0, -1) * 1);
-        console.log(eu_numerial_value);
-    
-        eu = ((eu_numerial_value + (-100)) + "%"); 
-        console.log(eu);
+        case "-200%":
+            hightLightFourthBreadcrumb();
+            break;
 
-        document.getElementById("slideshow_slider").style.marginLeft = eu;
+        case "-100%":
+            hightLightThirdBreadcrumb();
+            break;
+
+        case "0%":
+            hightLightSecondBreadcrumb();
+            break;       
     }
-    else {
-        document.getElementById("slideshow_slider").style.marginLeft = "0%";
-    }    
+
+
+    // if (sliderMargin != "-300%") {
+    //     var numerial_value = (sliderMargin.slice(0, -1) * 1); 
+    //     sliderMargin = ((numerial_value + (-100)) + "%"); 
+
+    //     document.getElementById("slideshow_slider").style.marginLeft = sliderMargin;
+    // }
+    // else {
+    //     document.getElementById("slideshow_slider").style.marginLeft = "0%";
+    // }    
+
+    // var breadcrumbs = $(".sb");
+    // console.log(breadcrumbs.length);
    
 }
 
-var counter = 0;
 setInterval(() => {
-   moveslide();
-}, 5000);
+    moveSlider();
+}, 7000);
+
+//change slide
+function hightLightFirstBreadcrumb() {
+    $(".sb").css("background", "#ffffffb5");
+    $("#sb1").css("background", "#412718");
+    $(".slideshow_slider").css("margin-left", "0%");
+}
+function hightLightSecondBreadcrumb() {
+    $(".sb").css("background", "#ffffffb5");
+    $("#sb2").css("background", "#412718");
+    $(".slideshow_slider").css("margin-left", "-100%");
+}
+function hightLightThirdBreadcrumb() {
+    $(".sb").css("background", "#ffffffb5");
+    $("#sb3").css("background", "#412718");
+    $(".slideshow_slider").css("margin-left", "-200%");
+}
+function hightLightFourthBreadcrumb() {
+    $(".sb").css("background", "#ffffffb5");
+    $("#sb4").css("background", "#412718");
+    $(".slideshow_slider").css("margin-left", "-300%");
+}
